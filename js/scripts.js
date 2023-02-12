89cd41d934f623507ade2b78630a62d1e673f5a1
Billy Lee <codingbillylee@gmail.com>
2023-02-11 18:41:16 -0800

    Add js to show answer on webpage using DOM.

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,14 @@ function roboHandler(event) {
 function roboOutput(num) {
   let numArray = [];
   let tempNum = "";
+  let stringArray = "";
   for(let i = 0; i <= num; i++) {
     tempNum = i.toString();
     numArray.push(" " + roboEditor(tempNum));
   }
   let finalOutput = document.querySelector("#ans");
-  finalOutput.append(numArray);
+  stringArray = numArray.toString();
+  finalOutput.innerText = stringArray;
   document.querySelector(".hidden").removeAttribute("class");
 }
 
